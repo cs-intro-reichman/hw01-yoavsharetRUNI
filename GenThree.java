f6d072d1b2a8621fd274395bf73256e7791f6bb8
yoavsharetRUNI <yoav.sharet@post.runi.ac.il>
2023-12-23 16:38:45 +0200

    finished Gen3, before autotest

--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,20 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+		int min_limit = Integer.parseInt(args[0]); 
+		int max_limit = Integer.parseInt(args[1]);
+		
+		// generate rand values in the given range (max not inclusive) 
+		int rand_1 = (int)(Math.random()*(max_limit - min_limit)) + min_limit;
+		int rand_2 = (int)(Math.random()*(max_limit - min_limit)) + min_limit;
+		int rand_3 = (int)(Math.random()*(max_limit - min_limit)) + min_limit;
+		// finding minimum generated value
+		int min_rand = Math.min(rand_1, Math.min(rand_2, rand_3));
+
+		//output
+		System.out.println(rand_1);
+		System.out.println(rand_2);
+		System.out.println(rand_3);
+		System.out.println("The minimal generated number was " + min_rand);
 	}
 }
