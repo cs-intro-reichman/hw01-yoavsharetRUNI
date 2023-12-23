ec996eaee5d13d15beea0df7f513fff731486d96
yoavsharetRUNI <yoav.sharet@post.runi.ac.il>
2023-12-23 16:13:28 +0200

    finished Triangle, before autotest

--- Triangle.java
+++ Triangle.java
@@ -5,6 +5,13 @@
  */ 
 public class Triangle {
 	public static void main(String[] args) {
-		// Put your code here	
+		int a = Integer.parseInt(args[0]);	
+		int b = Integer.parseInt(args[1]);
+		int c = Integer.parseInt(args[2]);
+
+		boolean is_triangle = (a + b > c) && (b + c > a) && (a + c > b);
+		String output = a + ", " + b + ", " + c + ": " + is_triangle;
+
+		System.out.println(output);
 	}
 }
